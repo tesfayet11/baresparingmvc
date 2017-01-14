c832defa3901aa45c6070d90c8b64866c1a273d9
Tesfaye Weldegiorgis <tweldegiorgis@Tesfayes-MacBook-Pro.local>
2017-01-14 15:44:01 -0600

    add a spring mvc web project with java configuration

---
+++ src/main/java/com/packt/webstore/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.packt.webstore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.packt.webstore")
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+	@Bean
+	public InternalResourceViewResolver viewResolver(){
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+}
